726066d594e717a47af304dab76d076ee8fa9ebf
Shivam Bansal <shivam5992@gmail.com>
2017-06-30 19:47:30 +0530

    updated ticker and heatmap

--- configs.py
+++ configs.py
@@ -1,15 +1,60 @@
+def ceo_ticker():
+	if tick == "23":
+		return "Sundar Pichai (2015 - Present)"
+	elif tick == "16":
+		return "Larry Page (2011 - 2015)"
+	elif tick == "11":
+		return "Eric Shmidt (2011 - 2011)"
+
+
+def year_ticker():
+	if tick == "23":
+		return "Sundar Pichai (2015 - Present)"
+	elif tick == "16":
+		return "Larry Page (2011 - 2015)"
+	elif tick == "11":
+		return "Eric Shmidt (2011 - 2011)"
+
+
 ceo = {	'chart_title' : "Google Acquisitions by CEOs",
 		'category' : 'ceo',
-		'data_config' : [{ 'name' : 'Eric Shmidt',
-						  	'year' : '(2001 - 2011)',
+		'ticks' : [23,16,11],
+		'ticker_func' : ceo_ticker,
+		'data_config' : [{ 'name' : 'Eric Shmidt (2001 - 2011)',
 							'starting_year' : 2000,
-							'dataset' : []},
-						  { 'name' : 'Larry Page',
-						    'year' : '(2011 - 2015)',
+							'dataset' : [],
+							'upper_index' : 24},
+						  { 'name' : 'Larry Page (2011 - 2015)',
 							'starting_year' : 2011,
-							'dataset' : []},
-						  { 'name' : 'Sundar Pichai' ,
-			 	    		'year' : '(2015 - Present)',
+							'dataset' : [],
+							'upper_index' : 18},
+						  { 'name' : 'Sundar Pichai (2015 - Present)',
 							'starting_year' : 2015,
-							'dataset' : []}]
+							'dataset' : [],
+							'upper_index' : 11}]
+	}
+
+year = {'chart_title' : "Google Acquisitions by Years",
+		'category' : 'year',
+		'ticks'  : [22,18,12, 7, 4],
+		'data_config' : [{  'name' : '2000 - 2004',
+							'starting_year' : 2000,
+							'dataset' : [],
+							'upper_index' : 4},
+						  { 'name' : '2004 - 2008',
+							'starting_year' : 2004,
+							'dataset' : [],
+							'upper_index' : 8},
+						  { 'name' : '2008 - 2012' ,
+							'starting_year' : 2008,
+						 	'dataset' : [],
+						 	'upper_index' : 13},
+						  { 'name' : '2012 - 2016' ,
+							'starting_year' : 2012,
+							'dataset' : [],
+							'upper_index' : 19},
+						  { 'name' : '2016 - Present',
+							'starting_year' : 2016,
+							'dataset' : [],
+							'upper_index' : 22},]
 	}
--- heatmap_by_ceo.py
+++ heatmap_by_ceo.py
@@ -5,7 +5,7 @@ from bokeh.palettes import Oranges9, Oranges9
 import pandas as pd 
 import csv 
 
-from configs import ceo
+from configs import ceo, year
 
 def dataset_preparation(row_data, upper_y, chunk_len = 20, max_color = 5):
 	input_data = list(reversed(row_data))
@@ -25,21 +25,12 @@ def dataset_preparation(row_data, upper_y, chunk_len = 20, max_color = 5):
 			z_coordinate = max_color - y_index
 			updated_row.append(z_coordinate)
 
-			updated_row.append(row[2])
-
 			dataset.append(updated_row)
 	return dataset
 
-def ticker():
-	if tick == "23":
-		return "Sundar Pichai (2015 - Present)"
-	elif tick == "16":
-		return "Larry Page (2011 - 2015)"
-	elif tick == "11":
-		return "Eric Shmidt (2011 - 2011)"
 
-def beautify_heatmap(html_object):
-	html_object.yaxis[0].ticker = FixedTicker(ticks=[23,16,11])
+def beautify_heatmap(html_object, ticks, ticker_func):
+	html_object.yaxis[0].ticker = FixedTicker(ticks = ticks)
 
 	html_object.xaxis.axis_label = ""
 	html_object.yaxis.axis_label = ""
@@ -49,7 +40,7 @@ def beautify_heatmap(html_object):
 	html_object.outline_line_color = "white"
 	html_object.yaxis.major_label_text_color = "black"
 
-	html_object.yaxis.formatter = FuncTickFormatter.from_py_func(ticker)	
+	html_object.yaxis.formatter = FuncTickFormatter.from_py_func(ticker_func)	
 	return html_object
 
 def read_dataset(chart_config):
@@ -69,30 +60,53 @@ def read_dataset(chart_config):
 
 			year = int(row[1])
 
-			# Customize this !!!!!!!!!!!!!!!!!!!!!!!!!!
-			if year >= data_config[2]['starting_year']:
-				data_config[2]['dataset'].append(row)
-			elif year >= data_config[1]['starting_year']:
-				data_config[1]['dataset'].append(row)
-			else:
-				data_config[0]['dataset'].append(row)
+			# Improve this !!!!!!!!!!!!!!!!!!!!!!!!!!
+
+			if len(data_config) == 3:
+				if year >= data_config[2]['starting_year']:
+					data_config[2]['dataset'].append(row)
+				elif year >= data_config[1]['starting_year']:
+					data_config[1]['dataset'].append(row)
+				else:
+					data_config[0]['dataset'].append(row)
+
+			elif len(data_config) == 4:
+				if year >= data_config[3]['starting_year']:
+					data_config[3]['dataset'].append(row)
+				elif year >= data_config[2]['starting_year']:
+					data_config[2]['dataset'].append(row)
+				elif year >= data_config[1]['starting_year']:
+					data_config[1]['dataset'].append(row)
+				else:
+					data_config[0]['dataset'].append(row)
+
+			elif len(data_config) == 5:
+				if year >= data_config[4]['starting_year']:
+					data_config[4]['dataset'].append(row)
+				elif year >= data_config[3]['starting_year']:
+					data_config[3]['dataset'].append(row)
+				elif year >= data_config[2]['starting_year']:
+					data_config[2]['dataset'].append(row)
+				elif year >= data_config[1]['starting_year']:
+					data_config[1]['dataset'].append(row)
+				else:
+					data_config[0]['dataset'].append(row)
 
 	chart_config['data_config'] = data_config
+	chart_config['header'] = header
 	return chart_config
 
 def prepare_heatmap(chart_config):
 	data_config = chart_config['data_config']
+	ticks = chart_config['ticks']
+	ticker_func = chart_config['ticker_func']
 
-	data_config[0]['dataset'] = dataset_preparation(data_config[0]['dataset'], 24)
-	data_config[1]['dataset'] = dataset_preparation(data_config[1]['dataset'], 18)
-	data_config[2]['dataset'] = dataset_preparation(data_config[2]['dataset'], 11)
-	
 	by_category = []
-	by_category.extend(data_config[0]['dataset'])
-	by_category.extend(data_config[1]['dataset'])
-	by_category.extend(data_config[2]['dataset'])
+	for i in range(len(data_config)):
+		data_config[i]['dataset'] = dataset_preparation(data_config[i]['dataset'], data_config[i]['upper_index'])
+		by_category.extend(data_config[i]['dataset'])
 
-	inpDF = pd.DataFrame(by_category, columns = header)
+	inpDF = pd.DataFrame(by_category, columns = chart_config['header'])
 
 	# Not Working  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	hover_object = HoverTool(tooltips="""<div><span style="font-size: 17px;">@company</span></div>""")
@@ -101,12 +115,10 @@ def prepare_heatmap(chart_config):
 								 legend=False, palette=Oranges9, tools=[hover_object],
 								 title=chart_config['chart_title'], toolbar_location="above")
  
-	html_object = beautify_heatmap(html_object)
+	html_object = beautify_heatmap(html_object, ticks, ticker_func)
 	output_file("outputs/heatmap_" + chart_config['category'] + ".html", title=chart_config['chart_title'])
 	show(html_object)
 
 if __name__ == '__main__':
-
-	
 	chart_config = read_dataset(ceo)
-	prepare_heatmap(data_config)
+	prepare_heatmap(chart_config)
